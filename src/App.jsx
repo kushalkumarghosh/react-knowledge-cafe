7f1e05618ab88771411bfce76cbdc4416324ad5e
kushalkumarghosh <cse.kushal1@gmail.com>
2023-11-20 02:43:33 +0600

    add Bookmark button and lift up the state

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,22 @@ import './App.css'
 import Blogs from './components/Blogs/Blogs'
 import Header from './components/Header/Header'
 import Bookmarks from './components/Bookmarks/Bookmarks'
+import { useState } from 'react'
 
 function App() {
+
+  const [bookmarks, setBookmarks] = useState([]);
+  const handleAddToBookmark = blog =>{
+    console.log('bookmark added')
+  }
   
 
   return (
     <>
 
       <Header></Header>
-      <div className='md:flex'>
-      <Blogs></Blogs>
+      <div className='md:flex max-w-7xl mx-auto'>
+      <Blogs handleAddToBookmark={handleAddToBookmark}></Blogs>
       <Bookmarks></Bookmarks>
 
       </div>
--- src/components/Blog/Blog..jsx
+++ src/components/Blog/Blog..jsx
@@ -1,19 +1,15 @@
 import PropTypes from "prop-types";
+import { FaBookmark } from "react-icons/fa";
 
-const Blog = ({ blog }) => {
-  const {
-    title,
-    cover,
-    author,
-    author_img,
-    reading_time,
-    posted_date,
-    hashtags,
-  } = blog;
+
+const Blog = ({ blog, handleAddToBookmark }) => {
+  const { title, cover, author, author_img, reading_time, posted_date, 
+    hashtags} = blog;
   return (
-    <div>
-      <img src={cover} alt={`Cover picture of the title ${title}`} />
-      <div className="flex justify-between">
+    <div className="mb-20">
+      <img className="w-full mb-8" src={cover} alt={`Cover picture of the title 
+      ${title}`} />
+      <div className="flex justify-between mb-4">
         <div className="flex">
           <img className="w-14" src={author_img} alt="" />
           <div className="ml-6">
@@ -23,6 +19,9 @@ const Blog = ({ blog }) => {
         </div>
         <div>
           <span>{reading_time} min read</span>
+          <button
+          onClick={handleAddToBookmark}
+           className="ml-2 text-xl text-red-600"><FaBookmark></FaBookmark></button>
         </div>
       </div>
       <h2 className="text-4xl">{title}</h2>
